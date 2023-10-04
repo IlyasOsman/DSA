197c13b559c66a324c55e10caa8b5ce2fb7f158e
Ilyas Osman <earlyosman@gmail.com>
2023-10-04 20:32:29 +0000

    Summ of all divisors

---
+++ Step 1 : Learn the basics/Step 1.4 : Know Basic Maths/SumOfAllDivisors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+int sumOfAllDivisors(int n){
+	// Write your code here.
+	  int totalSum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int divisorSum = 0;
+
+        for (int j = 1; j*j <= i; j++) {
+            if (i % j == 0) {
+                divisorSum += j;
+
+                if (i / j != j) {
+                    divisorSum += i / j;
+                }
+            }
+        }
+        totalSum += divisorSum;
+    }
+
+    return totalSum;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    sumOfAllDivisors(n);
+
+    return 0;
+}
