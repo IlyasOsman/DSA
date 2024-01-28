2f6b61ca83c1a02c2dc5b4f373147d96c1c6be98
ilyasosman <earlyosman@gmail.com>
2024-01-29 00:19:47 +0300

    ...

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
@@ -350,3 +350,59 @@ vector<int> sortedArray(vector<int> a, vector<int> b)
             return (n * (n + 1) / 2) - sum;
         }
     };
+
+    /*
+        Time Complexity : O( N )
+        Space complexity: O( 1 )
+
+        Where N is the size of the vehicle array.
+    */
+
+    int traffic(int n, int m, vector<int> vehicle)
+    {
+        // Create ans, count and right, and assign with 0.
+        int ans = 0, count = 0, right = 0;
+        // For loop to move the left pointer.
+        for (int left = 0; left < n; left++)
+        {
+            // while right is less than n and count is <= m.
+            while (right < n && count <= m)
+            {
+                // If vehicle at right is 0.
+                if (vehicle[right] == 0)
+                {
+                    count++;
+                    // Break if count exceeds m.
+                    if (count > m)
+                    {
+                        count--;
+                        break;
+                    }
+                }
+                right++;
+            }
+            // Update answer.
+            ans = max(ans, right - left);
+            // If vehicle at left is 0.
+            if (vehicle[left] == 0)
+            {
+                count--;
+            }
+        }
+        // Return answer.
+        return ans;
+    }
+
+// occur once
+#include <vector>
+
+    int getSingleElement(vector<int> & arr)
+    {
+        // Write your code here.
+        int result = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            result = result ^ arr[i];
+        }
+        return result;
+    }
