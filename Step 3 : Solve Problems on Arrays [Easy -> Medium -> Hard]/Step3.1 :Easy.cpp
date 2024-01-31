b6154e0ef5cd449b628585a837cb61d9466ab5f7
ilyasosman <earlyosman@gmail.com>
2024-01-31 21:06:13 +0300

    hashmap

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
@@ -490,3 +490,43 @@ vector<int> sortedArray(vector<int> a, vector<int> b)
         }
         return maxL;
     }
+
+#include <bits/stdc++.h>
+    // Handles negative no.
+    int getLongestSubarray(vector<int> & nums, int k)
+    {
+        // Write your code here
+        int n = nums.size();
+        map<int, int> preSumMap;
+        int sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++)
+        {
+            // calculate the prefix sum till index i:
+            sum += nums[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k)
+            {
+                maxLen = max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            int rem = sum - k;
+
+            // Calculate the length and update maxLen:
+            if (preSumMap.find(rem) != preSumMap.end())
+            {
+                int len = i - preSumMap[rem];
+                maxLen = max(maxLen, len);
+            }
+
+            // Finally, update the map checking the conditions:
+            if (preSumMap.find(sum) == preSumMap.end())
+            {
+                preSumMap[sum] = i;
+            }
+        }
+
+        return maxLen;
+    }
