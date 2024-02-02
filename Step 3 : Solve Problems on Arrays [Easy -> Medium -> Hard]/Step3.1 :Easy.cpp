7b1b3ef96580b9d96c1cee5fde7e1c338d600ecc
ilyasosman <earlyosman@gmail.com>
2024-02-02 23:30:05 +0300

    2sum

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step3.1 :Easy.cpp
@@ -565,3 +565,31 @@ vector<int> sortedArray(vector<int> a, vector<int> b)
             return count <= 1;
         }
     };
+
+    /*
+        Time Complexity : O( N )
+        Space complexity: O( N )
+
+        Where N is the size of array.
+    */
+
+#include <unordered_map>
+
+    string read(int n, vector<int> book, int target)
+    {
+        // Create a hash-map.
+        unordered_map<int, bool> mp;
+        // Traverse through the array.
+        for (int itr = 0; itr < n; itr++)
+        {
+            // If book with remaining pages is present.
+            if (mp.find(target - book[itr]) != mp.end())
+            {
+                return "YES";
+            }
+            // Mark current book as true.
+            mp[book[itr]] = true;
+        }
+        // If no book pair is possible.
+        return "NO";
+    }
