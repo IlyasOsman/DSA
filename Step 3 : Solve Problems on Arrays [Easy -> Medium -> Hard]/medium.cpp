268d5ce81c51089c0c5b169003d540272826d601
ilyasosman <earlyosman@gmail.com>
2024-02-05 16:54:39 +0300

    kadane algo

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
@@ -96,19 +96,51 @@ int majorityElement(vector<int> v)
         }
     }
 
-    int count1 = 0;
+    int countMax = 0;
     for (int i = 0; i < n; i++)
     {
         if (v[i] == element)
         {
-            count1++;
+            countMax++;
         }
     }
 
-    if (count1 > (n / 2))
+    if (countMax > (n / 2))
         return element;
 
     return -1;
+}
+
+// Kadane's algo.
+
+long long maxSubarraySum(int arr[], int n)
+{
+    long long maxi = LONG_MIN; // maximum sum
+    long long sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        sum += arr[i];
+
+        if (sum > maxi)
+        {
+            maxi = sum;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0)
+        {
+            sum = 0;
+        }
+    }
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    // if (maxi < 0) maxi = 0;
+
+    return maxi;
 }
 
 int main()
