126116cc4020cd822a1a65617544a7a120c78509
ilyasosman <earlyosman@gmail.com>
2024-02-09 22:43:06 +0300

    rotate array by 90 deg

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
@@ -445,10 +445,31 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
     return matrix;
 }
 
+
+// Transpose and reverse.
+
+void rotateMatrix(vector<vector<int>> &mat){
+	// Write your code here.
+	int n = mat.size();
+
+	for(int i = 0; i < n -1; i++) {
+		for(int j = i + 1; j < n; j++) {
+			swap(mat[i][j], mat[j][i]);
+		}
+	}
+
+	for(int i = 0; i < n; i++) {
+		reverse(mat[i].begin(), mat[i].end());
+	}
+
+}
+
 int main()
 {
     int n = 6;
     vector<int> arr = {0, 2, 1, 2, 0, 1};
+
+
     sortArray(arr, n);
     cout << "After sorting:" << endl;
     for (int i = 0; i < n; i++)
