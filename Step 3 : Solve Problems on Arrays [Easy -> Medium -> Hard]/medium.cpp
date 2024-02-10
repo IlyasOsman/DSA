387e9befe47276b77d55a4e14a429f11b2054eb5
ilyasosman <earlyosman@gmail.com>
2024-02-10 22:22:45 +0300

    print spiral matrix

--- Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/medium.cpp
@@ -445,30 +445,80 @@ vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m)
     return matrix;
 }
 
-
 // Transpose and reverse.
 
-void rotateMatrix(vector<vector<int>> &mat){
-	// Write your code here.
-	int n = mat.size();
+void rotateMatrix(vector<vector<int>> &mat)
+{
+    // Write your code here.
+    int n = mat.size();
 
-	for(int i = 0; i < n -1; i++) {
-		for(int j = i + 1; j < n; j++) {
-			swap(mat[i][j], mat[j][i]);
-		}
-	}
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
 
-	for(int i = 0; i < n; i++) {
-		reverse(mat[i].begin(), mat[i].end());
-	}
+    for (int i = 0; i < n; i++)
+    {
+        reverse(mat[i].begin(), mat[i].end());
+    }
+}
 
+vector<int> printSpiral(vector<vector<int>> mat)
+{
+
+    // Define ans array to store the result.
+    vector<int> ans;
+
+    int n = mat.size();    // no. of nows
+    int m = mat[0].size(); // no. of columns
+
+    // Initialize the pointers reqd for traversal.
+    int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+    // Loop until all elements are not traversed.
+    while (top <= bottom && left <= right)
+    {
+
+        // For moving left to right
+        for (int i = left; i <= right; i++)
+            ans.push_back(mat[top][i]);
+
+        top++;
+
+        // For moving top to bottom.
+        for (int i = top; i <= bottom; i++)
+            ans.push_back(mat[i][right]);
+
+        right--;
+
+        // For moving right to left.
+        if (top <= bottom)
+        {
+            for (int i = right; i >= left; i--)
+                ans.push_back(mat[bottom][i]);
+
+            bottom--;
+        }
+
+        // For moving bottom to top.
+        if (left <= right)
+        {
+            for (int i = bottom; i >= top; i--)
+                ans.push_back(mat[i][left]);
+
+            left++;
+        }
+    }
+    return ans;
 }
 
 int main()
 {
     int n = 6;
     vector<int> arr = {0, 2, 1, 2, 0, 1};
-
 
     sortArray(arr, n);
     cout << "After sorting:" << endl;
