952c072552e0c9b0e30cb68e742ea4510bedbed8
ilyasosman <earlyosman@gmail.com>
2024-03-16 23:12:38 +0300

    ..

--- Step 4 : Binary Search [1D, 2D Arrays, Search Space]/Step4.1:BSon1DArrays.cpp
+++ Step 4 : Binary Search [1D, 2D Arrays, Search Space]/Step4.1:BSon1DArrays.cpp
@@ -519,3 +519,133 @@ int singleNonDuplicate(vector<int> &arr)
 
     return -1;
 }
+
+/*
+    Time Complexity: O(log 'n')
+    Space Complexity: O(1)
+
+    Where 'n' is the number of elements
+*/
+
+int findPeakElement(vector<int> &arr)
+{
+    // Write your code here
+    int n = arr.size();
+
+    if (arr[0] > arr[1])
+        return 0;
+    if (arr[n - 1] > arr[n - 2])
+        return n - 1;
+
+    int low = 1;
+    int high = n - 2;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+            return mid;
+
+        if (arr[mid] < arr[mid + 1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+// LOg n
+
+int floorSqrt(int n)
+{
+    // Write your code here.
+    int low = 1;
+    int high = n;
+
+    while (low <= high)
+    {
+        long long mid = (low + high) / 2;
+
+        long long square = (mid * mid);
+
+        if (square <= (long long)(n))
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return high;
+}
+
+/*
+    Time Complexity: O( sqrt(n) )
+    Space Complexity: O( 1 )
+
+    Where 'n' is the given number.
+*/
+
+// Function to find the square root of 'n'
+int floorSqrt(int n)
+{
+
+    // Corner case
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    // Take 'ans' variable to store the square root of given number 'n'
+    int ans = 1;
+
+    // Run loop while square of 'ans' is less than equal to 'n'
+    while (ans <= (n / ans))
+    {
+        // Increament 'ans' by 1
+        ans++;
+    }
+
+    // Return 'ans'-1
+    return (ans - 1);
+}
+
+int func(int mid, int n, int m)
+{
+    long long ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * mid;
+        if (ans > m)
+            return 2;
+    }
+    if (ans == m)
+        return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m)
+{
+    // Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1)
+        {
+            return mid;
+        }
+        else if (midN == 0)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
